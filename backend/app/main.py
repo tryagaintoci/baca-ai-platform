8cc2fb3745bd7c4478688734bbbeb09eb2a79700
tryagaintoci <tryagaintoci@gmail.com>
2026-07-31 08:33:52 +0200

    Format app/main.py with Black

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI(
-    title="BACA AI Platform",
-    version="0.1.0"
-)
+app = FastAPI(title="BACA AI Platform", version="0.1.0")
 
 
 @app.get("/")
 def root():
-    return {
-        "message": "Welcome to BACA AI Platform"
-    }
+    return {"message": "Welcome to BACA AI Platform"}
